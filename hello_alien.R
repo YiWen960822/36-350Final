b310eb733bcd139c426f9d4d94c04720e4a0b8d3
Yi Wen <wenyi_sophia@outlook.com>
2017-12-10 18:44:09 -0500

    Message to alien friends

---
+++ hello_alien.R
@@ -0,0 +1,28 @@
+print("
+      Dear extraterrestrial neighbour,
+      I am a universe dweller. I live on a planet called earth, which is located in the Solar System,
+      Milky Way. On earth, we call our species Human (and I am one of the human species), and humans are
+      the only intelligent species on our planet so far. We have a history of around 200,000 years, of
+      which a year is the period of Earth orbitting the Sun (the center of Solar System). According to
+      human civilization, Earth is categorized into 7 continents and 5 oceans. The human species are 
+      categorized by nationality, ethnicity and language. By nationality, we have around 220 nations 
+      (countries) on earth. Each country is independent with its own autonomy, army, public appearance, 
+      political system (rules and regulation), economic system (trade, exchange and currency) and social 
+      value (ethics and virtue). By ethnicity, human species have groups of people that have different 
+      skin color. By language, we speak 6909 living language (and there are many languages that have 
+      been used by our ancestors but are now not used or evolving). Some of us speak more than 1 
+      language and the most used languages are Chinese, English, Spanish, Hindi and Arabic. For 
+      communication, we usually convert all languages into English for people who don't know each 
+      other's language, but this rule varies in different regions too (officer in Europe covert languages
+      into French). To grow and thrive, human species has developed many artificial systems other than
+      nature. Politics guard the fundamental ideology of a group, economics guard exchange and trade 
+      of resources. Law rules and regulate any artificial system. Furthermore, advances in technology
+      associate closely with civilization: the way we live, how we assume the universe and our knowledge 
+      of truth. When there is great technology advances, there usually is a social, political and economic
+      reform, so that the artificial systems catches up with the new level of civilization. The reform
+      sometimes comes with violences, such as war in which groups of people fight with weapons. Currently,
+      human species is under a peace era, and we strive to keep the peace by unifying our species. The
+      most urgent problem we face, is that our activity on earth to thrive our own civilization is damaging
+      our planet. The reason behind it is that our artificial systems to guard our own prosperity do not
+      take 
+      ")
